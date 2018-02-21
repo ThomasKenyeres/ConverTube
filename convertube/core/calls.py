c7ade9f4cdaa08b9042a54df61c6a0bca5193f22
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-21 16:16:22 +0100

    Implemented: download video

--- convertube/core/calls.py
+++ convertube/core/calls.py
@@ -1,3 +1,6 @@
+import os
+
+from convertube.core import cli_helper
 from convertube.core.enums import VideoFormat
 
 
@@ -6,7 +9,29 @@ def yt_url_is_valid(url):
     return True
 
 def download_video(url, dl_path, video_format=VideoFormat.MP4):
-    pass
+    os.chdir(dl_path)
+    video_title_cmd = ["youtube-dl", "--get-filename", "-o", "%(title)s", str(url)]
+    video_name_cmd = ["youtube-dl", "--get-filename", "-o", "%(title)s.%(ext)s", str(url)]
+    video_ext_cmd = ["youtube-dl", "--get-filename", "-o", "%(ext)s", str(url)]
+
+    video_title = str(cli_helper.run_command(video_title_cmd)).strip()
+    video_name = str(cli_helper.run_command(video_name_cmd)).strip()
+    video_ext = str(cli_helper.run_command(video_ext_cmd)).strip()
+    filename = str(video_title) + "." + str(video_format)
+    print(str(video_title) + " " + str(video_format) + " >>> >>> " + str(filename))
+
+    # TODO: use youtube-dl python library
+    cmd = ["youtube-dl", "-f", str(video_format), "-o", str(video_title) + "." + str(video_format), str(url)]
+
+    out = str(cli_helper.run_command(cmd))
+
+    return {
+        "title": video_title,
+        "name": video_name,
+        "path": dl_path,
+        "requested_format": video_format,
+        "filename": filename
+    }
 
 def convert_video_to_mp3(video_path, new_mp3_path):
     pass
