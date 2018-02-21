ba5b78bfdaa8bfc91159841cacfc767744068b1f
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-21 16:59:59 +0100

    setup.py added(will be modified later)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ConverTube-core',
+    version='1.0',
+    packages=['convertube', 'convertube.core'],
+    url='',
+    license='',
+    author='thomas',
+    author_email='',
+    description='YouTube media downloader and converter headless core library'
+)
