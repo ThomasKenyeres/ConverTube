a099f254065c04ff949c7a1409e8f024ff00bc2c
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-21 16:22:01 +0100

    Some high level request defined

---
+++ convertube/api.py
@@ -0,0 +1,14 @@
+from convertube.core.enums import VideoFormat, AudioFormat
+
+
+def download_and_convert(url, mp3_target_path,
+                         mp3_filename=None,
+                         video_format=VideoFormat.MP4, audio_format=AudioFormat.MP3,
+                         keep_video=False, video_path=None):
+    pass
+
+def collect_videos(urls, video_format=VideoFormat.MP4, video_path=None):
+    pass
+
+def convert_all_in_cache_to_mp3(path):
+    pass
