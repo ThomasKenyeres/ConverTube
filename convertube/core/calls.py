4501108a6f168337ae3deca8c2309568a8ce38da
ThomasKenyeres <kenyerest97@gmail.com>
2018-02-21 16:11:57 +0100

    Defined call/request methods for media handling

---
+++ convertube/core/calls.py
@@ -0,0 +1,16 @@
+from convertube.core.enums import VideoFormat
+
+
+def yt_url_is_valid(url):
+    #TODO: implement validation
+    return True
+
+def download_video(url, dl_path, video_format=VideoFormat.MP4):
+    pass
+
+def convert_video_to_mp3(video_path, new_mp3_path):
+    pass
+
+def empty_video_cache():
+    pass
+
